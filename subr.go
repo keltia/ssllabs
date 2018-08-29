8186142b98a294688d8a8a52020046f71e313008
Ollivier Robert <roberto@keltia.net>
2018-08-29 11:34:04 +0200

    Copy our API functions over.

--- subr.go
+++ subr.go
@@ -6,15 +6,142 @@ Package ssllabs contains SSLLabs-related functions.
 package ssllabs
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"net/url"
+	"strings"
+	"time"
+
+	"net/http"
 
 	"github.com/pkg/errors"
-	"net/http"
 )
 
 func myRedirect(req *http.Request, via []*http.Request) error {
 	return nil
+}
+
+// AddQueryParameters adds query parameters to the URL.
+func AddQueryParameters(baseURL string, queryParams map[string]string) string {
+	params := url.Values{}
+	for key, value := range queryParams {
+		params.Add(key, value)
+	}
+	return fmt.Sprintf("%s?%s", baseURL, params.Encode())
+}
+
+// prepareRequest insert all pre-defined stuff
+func (c *Client) prepareRequest(method, what string, opts map[string]string) (req *http.Request) {
+	var endPoint string
+
+	// This allow for overriding baseurl for tests
+	if c.baseurl != "" {
+		endPoint = fmt.Sprintf("%s/%s", c.baseurl, what)
+	} else {
+		endPoint = fmt.Sprintf("%s/%s", baseURL, what)
+	}
+
+	c.verbose("Options:\n%v", opts)
+	baseURL := AddQueryParameters(endPoint, opts)
+	c.debug("baseURL: %s", baseURL)
+
+	req, _ = http.NewRequest(method, baseURL, nil)
+
+	c.debug("req=%#v", req)
+
+	// We need these when we POST
+	if method == "POST" {
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Accept", "application/json")
+	}
+
+	return
+}
+
+func (c *Client) callAPI(word, cmd, sbody string, opts map[string]string) ([]byte, error) {
+	var body []byte
+
+	retry := 0
+
+	c.debug("callAPI")
+	req := c.prepareRequest(word, cmd, opts)
+	if req == nil {
+		return []byte{}, errors.New("req is nil")
+	}
+
+	c.debug("clt=%#v", c.client)
+	c.debug("opts=%v", opts)
+
+	// If we have a POST and a body, insert them.
+	if sbody != "" && word == "POST" {
+		body := []byte(sbody)
+		buf := bytes.NewReader(body)
+		req.Body = ioutil.NopCloser(buf)
+		req.ContentLength = int64(buf.Len())
+	}
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		c.debug("err=%#v", err)
+		return []byte{}, errors.Wrap(err, "1st call")
+	}
+	defer resp.Body.Close()
+
+	c.debug("resp=%#v", resp)
+
+	for {
+		if retry == c.retries {
+			return nil, errors.New("retries")
+		}
+
+		c.debug("read body")
+		body, err = ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return []byte{}, errors.Wrapf(err, "body read, retry=%d", retry)
+		}
+
+		c.debug("body=%v", string(body))
+
+		if resp.StatusCode == http.StatusOK {
+
+			c.debug("status OK")
+
+			// We wait for FINISHED state
+			if !strings.Contains(string(body), "FINISHED") {
+				time.Sleep(2 * time.Second)
+				retry++
+				resp, err = c.client.Do(req)
+				if err != nil {
+					return body, errors.Wrapf(err, "pending, retry=%d", retry)
+				}
+				c.debug("resp was %v", resp)
+			} else {
+				return body, nil
+			}
+		} else if resp.StatusCode == http.StatusFound {
+			str := resp.Header["Location"][0]
+
+			c.debug("Got 302 to %s", str)
+
+			req := c.prepareRequest(word, cmd, opts)
+			if err != nil {
+				return []byte{}, errors.Wrap(err, "redirect")
+			}
+
+			resp, err = c.client.Do(req)
+			retry++
+			if err != nil {
+				return []byte{}, errors.Wrap(err, "client.Do failed")
+			}
+			c.debug("resp was %v", resp)
+		} else {
+			return body, errors.Wrapf(err, "status: %v body: %q", resp.Status, body)
+		}
+	}
+	return body, err
 }
 
 // Display for one report
