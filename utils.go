e4e57af4b5cb93d1f7b1b25a7b3ad5fc45e452fd
Ollivier Robert <roberto@keltia.net>
2018-08-29 10:28:06 +0200

    Our usual utility functions.

---
+++ utils.go
@@ -0,0 +1,40 @@
+// utils.go
+//
+// Copyright 2018 © by Ollivier Robert <roberto@keltia.net>
+
+package ssllabs
+
+import (
+	"fmt"
+	"log"
+	"time"
+)
+
+// Private area
+
+func toDuration(t int) time.Duration {
+	s := fmt.Sprintf("%ds", t)
+	d, _ := time.ParseDuration(s)
+	return d
+}
+
+// debug displays only if fDebug is set
+func (c *Client) debug(str string, a ...interface{}) {
+	if c.level >= 2 {
+		log.Printf(str, a...)
+	}
+}
+
+// debug displays only if fVerbose is set
+func (c *Client) verbose(str string, a ...interface{}) {
+	if c.level >= 1 {
+		log.Printf(str, a...)
+	}
+}
+
+// debug displays only if fVerbose is set
+func (c *Client) fatal(str string, a ...interface{}) {
+	if c.level >= 1 {
+		log.Fatalf(str, a...)
+	}
+}
