a6dc434f5452e739f2c31f64c093788aee9998bb
Ollivier Robert <roberto@keltia.net>
2018-08-30 11:46:35 +0200

    Add tests for AddQueryParameters().

--- subr_test.go
+++ subr_test.go
@@ -1,7 +1,21 @@
 package ssllabs
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestParseResults(t *testing.T) {
 
+}
+
+func TestAddQueryParameters(t *testing.T) {
+	p := AddQueryParameters("", map[string]string{"": ""})
+	assert.Equal(t, "?=", p)
+}
+
+func TestAddQueryParameters_2(t *testing.T) {
+	p := AddQueryParameters("foo", map[string]string{"bar": "baz"})
+	assert.Equal(t, "foo?bar=baz", p)
 }
