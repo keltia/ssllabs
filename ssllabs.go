3966d64fb9eeb153228431af5239f71d2f6b05e7
Ollivier Robert <roberto@keltia.net>
2019-12-03 11:37:08 +0100

    Welcome v0.14.2 with 529 handling.

--- ssllabs.go
+++ ssllabs.go
@@ -28,7 +28,7 @@ const (
 	DefaultRetry = 5
 
 	// MyVersion is the API version
-	MyVersion = "0.14.0"
+	MyVersion = "0.14.2"
 
 	// MyName is the name used for the configuration
 	MyName = "ssllabs"
